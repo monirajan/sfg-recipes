148bdd0c00bce3cdd5f65ed77468035db55af808
monirajan <a.monisha.1995@gmail.com>
2020-12-30 22:01:10 +0530

    Initial project creation

---
+++ src/test/java/guru/springframework/sfgrecipes/SfgRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgrecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgRecipesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
