1cfe1d87fcbb6cf6755c036e5a97ca3bcbd1fc8f
monirajan <a.monisha.1995@gmail.com>
2021-02-08 17:08:01 +0530

    Done some changes to make the db connection

--- src/main/java/guru/springframework/sfgrecipes/SfgRecipesApplication.java
+++ src/main/java/guru/springframework/sfgrecipes/SfgRecipesApplication.java
@@ -2,8 +2,12 @@ package guru.springframework.sfgrecipes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@EntityScan(basePackages = {"guru.springframework.domain"})  // scan JPA entities
 public class SfgRecipesApplication {
 
     public static void main(String[] args) {
