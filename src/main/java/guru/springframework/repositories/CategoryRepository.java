6d54bdd6d088787edb84df9afbbdc91243634931
monirajan <a.monisha.1995@gmail.com>
2021-02-08 16:18:24 +0530

    JPA query methods

--- src/main/java/guru/springframework/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -4,6 +4,9 @@ import guru.springframework.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
