08f227ea9545394e85259facf321cf7ea61f736e
monirajan <a.monisha.1995@gmail.com>
2021-02-20 21:32:41 +0530

    Added some unit tests

---
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -0,0 +1,36 @@
+package guru.springframework.controllers;
+
+import guru.springframework.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class IndexControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    IndexController controller;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        controller = new IndexController(recipeService);
+    }
+
+    @Test
+    void getIndexPage() {
+        String viewName = controller.getIndexPage(model);
+        assertEquals("index", viewName);
+        verify(recipeService,times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"),anySet());
+    }
+}
